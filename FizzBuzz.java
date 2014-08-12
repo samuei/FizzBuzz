ddcdc57a3ac6547860720465003a6a4de9c19fd4
Samuel A. Lamb <samuei@ku.edu>
2014-08-11 19:32:00 -0500

    Update FizzBuzz.java
    * DocString added to main
    * Replaced Magic Number (user can now do longer or shorter FizzBuzz games by changing END_OF_FIZZBUZZ)
    
    Haven't been able to make sure that constant works, yet. If you're cribbing this for a job interview and you're too lazy to see if it works first, use the previous version.

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,7 +1,14 @@
 public class FizzBuzz {
+  /** Java implementation of FizzBuzz game.
+   * <p>
+   * [Lists the numbers from 1 to 100. If the number is divisible by 3, it replaces the name with
+   * Fizz. If the number is divisible by 5, it replaces the name with Buzz. If the number is divisibe
+   * by both 3 and 5, it replaces the name with FizzBuzz.]
+   */
+public static final int END_OF_FIZZBUZZ = 100;
 public static void main(String []args) {
   System.out.println("Let's play FizzBuzz!");
-  for (int i = 1; i<=100; i++) {
+  for (int i = 1; i<=END_OF_FIZZBUZZ; i++) {
     if (i%3 == 0) {
       System.out.print("Fizz");
       if (i%5 == 0) {
