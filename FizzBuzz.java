ab4f43a24cfdeed2e151696a1737551f1c63de47
Samuel A. Lamb <samuei@ku.edu>
2014-08-16 22:58:07 -0500

    Constants!
    Output strings and divisor values replaced with constants, in case you
    want to play FizzBuzz variants.

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,21 +2,25 @@ public class FizzBuzz {
   /** Java implementation of FizzBuzz game.
    * <p>
    * [Lists the numbers from 1 to 100. If the number is divisible by 3, it replaces the name with
-   * Fizz. If the number is divisible by 5, it replaces the name with Buzz. If the number is divisibe
+   * Fizz. If the number is divisible by 5, it replaces the name with Buzz. If the number is divisible
    * by both 3 and 5, it replaces the name with FizzBuzz.]
    */
 public static final int END_OF_FIZZBUZZ = 100;
+public static final String FIZZ = "Fizz";
+public static final int FIZZ_NUMBER = 3;
+public static final String BUZZ = "Buzz";
+public static final int BUZZ_NUMBER = 5;
 public static void main(String []args) {
   System.out.println("Let's play FizzBuzz!");
   for (int i = 1; i<=END_OF_FIZZBUZZ; i++) {
-    if (i%3 == 0) {
-      System.out.print("Fizz");
-      if (i%5 == 0) {
-        System.out.print("Buzz");
+    if (i%FIZZ_NUMBER == 0) {
+      System.out.print(FIZZ);
+      if (i%BUZZ_NUMBER == 0) {
+        System.out.print(BUZZ);
         }
       }
-    else if (i%5 == 0) {
-      System.out.print("Buzz");
+    else if (i%BUZZ_NUMBER == 0) {
+      System.out.print(BUZZ);
       }
     else {
       System.out.print(i);
